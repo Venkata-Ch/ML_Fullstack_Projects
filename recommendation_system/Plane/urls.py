0bede1cf3b9b012a68ff500349ea99b7d10057af
VenkataChoppa18 <51722249+Venkata-Ch@users.noreply.github.com>
2024-10-16 18:41:20 -0400

    .

--- recommendation_system/Plane/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('templates/index.html', view=views.flight_recommendation),
-]
-
--- recommendation_system/execute.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python manage.py data_load --pythonpath flights_data.csv &&
-python manage.py make_recommendations &&
-python manage.py runserver 0.0.0.0:8000
